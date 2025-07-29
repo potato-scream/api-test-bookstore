d805612771af5ca69ac6de35f72093e3dc805725
kreng <kr.englesson@gmail.com>
2025-07-29 17:34:51 +0100

    add Owner

--- src/test/java/tests/AccountTests.java
+++ src/test/java/tests/AccountTests.java
@@ -213,4 +213,6 @@ public class AccountTests extends TestBase {
 
     private record UserCleanupData(String userId, String token) {
     }
+
+
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,11 @@
 package tests;
+import config.CredentialsConfig;
 import io.restassured.RestAssured;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.BeforeAll;
 public class TestBase {
         public static String baseUri = "https://demoqa.com";
+    public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
         @BeforeAll
         static void setUp() {
