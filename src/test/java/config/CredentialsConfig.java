f62f7f03de1b10e0fac3aeaaadf712ab5bcb5250
kreng <kr.englesson@gmail.com>
2025-08-11 15:47:51 +0100

    fix login pass

--- src/test/java/config/CredentialsConfig.java
+++ src/test/java/config/CredentialsConfig.java
@@ -5,9 +5,9 @@ import org.aeonbits.owner.Config;
 @Config.Sources({"classpath:config/credentials.properties"})
 public interface CredentialsConfig extends Config {
 
-  @Key("login")
+  @org.aeonbits.owner.Config.Key("login")
   String login();
 
-  @Key("password")
+  @org.aeonbits.owner.Config.Key("password")
   String password();
 }
